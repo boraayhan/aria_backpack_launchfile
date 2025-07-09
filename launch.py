63ba8fd8b50f9aba2bc831e25195164232a428f9
boraayhan <160386284+boraayhan@users.noreply.github.com>
2025-07-09 15:49:52 -0600

    Update launch.py

--- launch.py
+++ launch.py
@@ -5,33 +5,22 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 
-
 def generate_launch_description():
     return LaunchDescription(
         [
             Node(package="rxjoy_pkg", executable="rxjoy_node"),
             IncludeLaunchDescription(
                 PythonLaunchDescriptionSource(
-                    PathJoinSubstitution([
-                        FindPackageShare("another_pkg"),  # Name of the package where the launch file exists
-                        "launch",                         # Subdirectory containing the launch file
-                        "another_launch_file.py"          # Name of the launch file
-                    ])
+                    PathJoinSubstitution(
+                        [
+                            FindPackageShare(
+                                "livox_ros_driver2"
+                            ),  # Name of the package where the launch file exists
+                            "launch_ROS2",  # Subdirectory containing the launch file
+                            "msg_MID360_launch.py",  # Name of the launch file
+                        ]
+                    )
                 )
-            )
-            """
-            EXAMPLE:
-            
-            Node(
-                package="package_name",
-                executable="node_name",
-                remappings=[
-                    ("/topic/remap1", "othertopic/remap1"),
-                    ("/topic/remap2", "othertopic/remap2"),
-                    ("/topic/remap3", "othertopic/remap3"),
-
-                ]
-            )
-            """,
+            ),
         ]
     )
